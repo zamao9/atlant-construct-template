f0d6aa77484b1ddec9567975d5b53dad777d3c53
zamao9 <mansory_09@mail.ru>
2023-04-05 16:29:08 +0300

    saffafa

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
 const burgerOpen = () => {
-  const header = document.querySelector('.header'),
-    headerBg = document.querySelector('.header__bg'),
+  const headerBg = document.querySelector('.header__bg'),
     burger = document.querySelector('.burger .burger__item'),
     nav = document.querySelector('.nav');
 
   if (burger) {
     burger.addEventListener('click', (e) => {
-      header.classList.toggle('active');
       headerBg.classList.toggle('active');
       nav.classList.toggle('active');
       e.currentTarget.classList.toggle('active');
     });
 
     headerBg.addEventListener('click', (e) => {
-      header.classList.remove('active');
       burger.classList.remove('active');
       nav.classList.remove('active');
       e.currentTarget.classList.remove('active');
