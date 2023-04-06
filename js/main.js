83ee0312cf9ebc8b1c41b47a2141edf26883a7c0
zamao9 <mansory_09@mail.ru>
2023-04-06 18:51:01 +0300

    afaafasf

--- js/main.js
+++ js/main.js
@@ -1,20 +1,58 @@
-const burgerOpen = () => {
-  const headerBg = document.querySelector('.header__bg'),
-    burger = document.querySelector('.burger .burger__item'),
-    nav = document.querySelector('.nav');
+document.addEventListener('DOMContentLoaded', () => {
+	/* Burger */
+	const burgerOpen = () => {
+		const headerBg = document.querySelector('.header__bg'),
+			burger = document.querySelector('.burger .burger__item'),
+			nav = document.querySelector('.nav'),
+			body = document.getElementsByTagName('body');
 
-  if (burger) {
-    burger.addEventListener('click', (e) => {
-      headerBg.classList.toggle('active');
-      nav.classList.toggle('active');
-      e.currentTarget.classList.toggle('active');
-    });
+		if (burger) {
+			burger.addEventListener('click', (e) => {
+				headerBg.classList.toggle('active');
+				nav.classList.toggle('active');
+				body[0].classList.toggle('block');
+				e.currentTarget.classList.toggle('active');
+			});
 
-    headerBg.addEventListener('click', (e) => {
-      burger.classList.remove('active');
-      nav.classList.remove('active');
-      e.currentTarget.classList.remove('active');
-    });
-  }
-};
-burgerOpen();
+			headerBg.addEventListener('click', (e) => {
+				burger.classList.remove('active');
+				nav.classList.remove('active');
+				body[0].classList.remove('block');
+				e.currentTarget.classList.remove('active');
+			});
+		}
+	};
+	burgerOpen();
+
+	/* Accordion */
+	const accordion = document.querySelectorAll('.accordion__items'),
+		arrow = document.querySelectorAll('.accordion__arrow'),
+		subtitle = document.querySelectorAll('.accordion__subtitle');
+
+	accordion.forEach((el) => {
+		el.addEventListener('click', (e) => {
+			const self = e.currentTarget,
+				control = self.querySelector('.accordion__head'),
+				content = self.querySelector('.accordion-body');
+
+			self.classList.toggle('open')
+	
+			if (self.classList.contains('open')) {
+				control.setAttribute('aria-expanded', true)
+				content.setAttribute('aria-hidden', false)
+				content.style.maxHeight = content.scrollHeight + 'px'
+				content.style.opacity = '1'
+			} else {
+				control.setAttribute('aria-expanded', false)
+				content.setAttribute('aria-hidden', true)
+				content.style.maxHeight = null
+				content.style.opacity = null
+			}
+		})
+	})
+
+
+
+
+
+})
